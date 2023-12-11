ebeff66d8de430f7c0844d8339bb09c759031396
RaquelRosenbaum <brightfutures7777@gmail.com>
2023-12-11 21:03:46 +0200

    GOLD

---
+++ Week3/Day2/2ExercisesXPGold/script.js
@@ -0,0 +1,54 @@
+// EXERCISE 1
+
+// Instructions
+// let sentence = ["my","favorite","color","is","blue"];
+// Write some simple Javascript code that will join all the items in the array above, and console.log the result.
+
+let sentence = ["my","favorite","color","is","blue"];
+let sentenceString=sentence.join();
+console.log(sentenceString);
+
+
+// EXERCISE 2
+
+// Instructions
+// Create 2 variables. The values should be strings. For example:
+// let str1 = "";
+// let str2 = "";
+
+
+// 2. Slice out and swap the first 2 characters of the two strings from part 1.
+
+
+// 3. Create a third variable where the value is the concatenation of the two strings from the part 1 (separated by a space).
+
+
+// 4. Finally console.log the new concatenated string.
+
+
+
+// Some Examples
+
+// let str1 = "mix";
+// let str2 = "pod";
+// let newWord should be equal to 'pox mid'
+
+// let firstWord = "Hello";
+// let secondWord = "World";
+// let thirdWord should be equal to 'Wollo Herld'
+
+
+// EXERCISE 3
+
+// Instructions
+// Make a Calculator. Follow the instructions:
+
+// Prompt the user for the first number.
+// Store the first number in a variable called num1.
+// Hint : console.log the type of the variable num1. What should you do to convert it to a number ?
+// Prompt the user for the second number.
+// Store the second number in a variable called num2.
+// Create an Alert where the value is the SUM of num1 and num2.
+// BONUS: Make a program that can subtract, multiply, and also divide!
+
+
