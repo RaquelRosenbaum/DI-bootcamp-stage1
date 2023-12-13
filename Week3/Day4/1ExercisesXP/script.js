8e2d2a943ef7b94570fd5ac2b6dbf5dd4c6ef596
RaquelRosenbaum <brightfutures7777@gmail.com>
2023-12-13 13:42:17 +0200

    Week3Day4ExercisesXP

---
+++ Week3/Day4/1ExercisesXP/script.js
@@ -0,0 +1,12 @@
+let x=5;
+let y=2;
+    // if typeof(x) != “number”){
+    //     prompt(“not a number)};
+    // if typeof(y) != "number"){
+    //     prompt(“not a number)};
+    if(x>y) {
+	    alert(“x is the bigger number”) }
+    else if(y>x) {
+	    alert(“y is the bigger number”)}
+    else(x=y) {
+	    alert(“the two numbers are equal”)}
