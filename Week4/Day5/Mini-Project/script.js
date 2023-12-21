b4aa2a6a28cf321923bc28c9957a59e9a8569fe0
RaquelRosenbaum <brightfutures7777@gmail.com>
2023-12-21 20:23:16 +0200

    miniproject

---
+++ Week4/Day5/Mini-Project/script.js
@@ -0,0 +1,18 @@
+function playTheGame() {
+    let yesnoplay = confirm("Do you want to play the game?")
+        if false {
+            alert("No problem, Goodbye."
+        } 
+        else {
+            let numbentrd = prompt("Enter a number between 0 and 10");
+            
+            if (typeof entnum !===number) {
+                alert("Sorry Not a number, Goodbye");//use isNan not typeof
+            else if (0<entnum!<10)
+            else ()
+            let computerNumber = math.floor(math.random()*10);
+            }
+        }
+    
+    }
+}
