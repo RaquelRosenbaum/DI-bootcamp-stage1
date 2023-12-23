55825b6476f5e42ee58d5b27351282ce9954e3ee
RaquelRosenbaum <brightfutures7777@gmail.com>
2023-12-23 21:27:47 +0200

    wk4dy2dailychall

---
+++ Week4/Day2/DailyChallenge/script.js
@@ -0,0 +1,8 @@
+
+    console.log("*********");
+    console.log("* Hello *");
+    console.log("* World *");
+    console.log("* in    *");
+    console.log("* a     *");
+    console.log("* frame *");
+    console.log("*********");
