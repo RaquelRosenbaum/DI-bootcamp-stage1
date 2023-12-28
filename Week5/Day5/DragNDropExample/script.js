0d9548046b09f42cce6a97af1bc3edbf64c0b5f7
RaquelRosenbaum <brightfutures7777@gmail.com>
2023-12-28 19:26:40 +0200

    editsclass

---
+++ Week5/Day5/DragNDropExample/script.js
@@ -0,0 +1,53 @@
+function onDragStart4PinkSquare(event) {
+    console.log("start");
+    event.dataTransfer.setData("text/plain", event.target.id)//store the address of the moved object in text form
+    //name, target, etc. instead of id possible
+    //dataTransfer is an object inside the event that stores the id of the element, the address of the element.  You need it because after the thing being dragged is dropped onto the other square, you need the ID to be appended to the new location, the orange square
+}
+function onDragEnd4PinkSquare(event) {
+    console.log("end");
+}
+function onDrag4PinkSquare(event) {
+    event.target.textContent = event.clientX + " " + event.clientY;
+    //coords of the top left - this shows the coords on the square itself instead of just in the console
+    //event.target is the element itself
+}
+
+//but we need to keep the address of the object being moved - global variable stores this - the event can store data inside it
+
+function onDragOver4OrangeSquare(event){
+    console.log("over orange");
+    event.preventDefault();
+}
+//without this the browser won't let us drop the box into the orange square//
+
+function onDrop4OrangeSquare(event) {
+    console.log("drop");
+    const id = event.dataTransfer.getData("text/plain");
+    console.log(id);
+}
+
+const pinkSquare = document.getElementById(id);
+    console.log(pinkSquare);
+
+//don't change the id for a DOM element unless you're really careful about it  because that messes everything up - you won't be able to find it
+
+event.target.appendChild(pinkSquare);//event.target is the id of the orange square
+
+//you can't drag it back because the original pink box isn't orange
+//so:
+
+function onDragOver4PinkSquare(event){
+    console.log("over orange");
+    event.preventDefault();
+}
+
+function onDrop4PinkSquare(event) {
+    console.log("drop");
+    const id = event.dataTransfer.getData("text/plain");
+    console.log(id);
+}
+
+const orangeSquare = document.getElementById(id);
+    console.log(orangeSquare);
+
