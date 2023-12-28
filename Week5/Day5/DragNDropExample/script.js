c00a5b2fba146b791f9e41d8b1c83e2da22dd49d
RaquelRosenbaum <brightfutures7777@gmail.com>
2023-12-28 20:52:52 +0200

    editsdragndrop

--- Week5/Day5/DragNDropExample/script.js
+++ Week5/Day5/DragNDropExample/script.js
@@ -25,29 +25,42 @@ function onDrop4OrangeSquare(event) {
     console.log("drop");
     const id = event.dataTransfer.getData("text/plain");
     console.log(id);
+    const pinkSquare = document.getElementById(id);
+    console.log(pinkSquare);
+    event.target.appendChild(pinkSquare);
 }
 
-const pinkSquare = document.getElementById(id);
-    console.log(pinkSquare);
 
 //don't change the id for a DOM element unless you're really careful about it  because that messes everything up - you won't be able to find it
 
-event.target.appendChild(pinkSquare);//event.target is the id of the orange square
+//event.target is the id of the orange square
 
 //you can't drag it back because the original pink box isn't orange
 //so:
 
+function onDragStart4OrangeSquare(event) {
+    console.log("start");
+    event.dataTransfer.setData("text/plain", event.target.id)//store the address of the moved object in text form
+    //name, target, etc. instead of id possible
+    //dataTransfer is an object inside the event that stores the id of the element, the address of the element.  You need it because after the thing being dragged is dropped onto the other square, you need the ID to be appended to the new location, the orange square
+}
+function onDragEnd4OrangeSquare(event) {
+    console.log("end");
+}
 function onDragOver4PinkSquare(event){
     console.log("over orange");
     event.preventDefault();
 }
 
-function onDrop4PinkSquare(event) {
+function onDropOver4PinkSquare(event) {
     console.log("drop");
     const id = event.dataTransfer.getData("text/plain");
     console.log(id);
+    const orangeSquare = document.getElementById(id);
+    console.log(orangeSquare);
+    event.target.appendChild(orangeSquare);
 }
 
-const orangeSquare = document.getElementById(id);
-    console.log(orangeSquare);
 
+
+//event.target is the id of the pink square
