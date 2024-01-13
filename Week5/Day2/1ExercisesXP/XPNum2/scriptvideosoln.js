e3e27f0daf8cd3e539b0c4c61aab8377312cb29b
RaquelRosenbaum <brightfutures7777@gmail.com>
2024-01-13 17:37:07 +0200

    wk5dy2edits

--- Week5/Day2/1ExercisesXP/XPNum2/scriptvideosoln.js
+++ Week5/Day2/1ExercisesXP/XPNum2/scriptvideosoln.js
@@ -3,29 +3,29 @@
 
 
 // INITIAL CODING PRE-REFACTORING:
-// const input = document.getElementById(“userinput”); 
-// const button = document.getElementById(“enter”); 
-// const ul = document.querySelector(“ul”);
+const input = document.getElementById("form"); 
+const button = document.getElementById("submit"); 
+const ul = document.querySelector("ul");
 
-// button.addEventListener(“click”, function() {
-// 	if (input.value.length > 0 {
-// 		let li = document.createElement(“li”);
-// 		li.appendChild(document.createTextNode(input.value));
-// 		ul.appendChild(li);
-// 		input.value = “”;
-// 	}
-// })
+button.addEventListener("click", function() {
+	if (input.value.length > 0) {
+		let li = document.createElement("li");
+		li.appendChild(document.createTextNode(input.value));
+		ul.appendChild(li);
+		input.value = "";
+	}
+})
 
-// input.addEventListener(“keypress”, function(event) {
-// 	if (input.value.length >0 && event.keyCode === 13) {
-// 		let li = document.createElement(“li”);
-// 		li.appendChild(document.createTextNode(input.value));
-// 		ul.appendChild(li);
-// 		input.value = “”;
-// 	}
-// })
+input.addEventListener("keypress", function(event) {
+	if (input.value.length >0 && event.keyCode === 13) {
+		let li = document.createElement("li");
+		li.appendChild(document.createTextNode(input.value));
+		ul.appendChild(li);
+		input.value = "";
+	}
+})
 
-//too much repetition, not as easy to understand the intent of the code, what it’s doing
+//More notes- too much repetition, not as easy to understand the intent of the code, what it’s doing
 
 
 // AFTER REFACTORING:
@@ -34,10 +34,10 @@
 // const button = document.getElementById(“enter”);  //the  enter button next to the entry field
 // const ul = document.querySelector(“ul”);
 
-// //the above are cached for repeated use throughout the rest of the code, during each function - less processor time, more concise code (DOM consumes a lot of processor time)
+//the above are cached for repeated use throughout the rest of the code, during each function - less processor time, more concise code (DOM consumes a lot of processor time)
 
 
-// //now below the four functions sit ready to run according to the two eventListeners at the end of the code.  They determine what the length of the entry is if any, create a list element
+//now below the four functions sit ready to run according to the two eventListeners at the end of the code.  They determine what the length of the entry is if any, create a list element
 
 
 // function inputLength() {
@@ -47,8 +47,8 @@
 // function createListElement() {
 // 	const li = document.createElement(“li”);  //creates list item, li, type element
 // 	li.appendChild(document.createTextNode(input.value));  //makes a text node into which it places the value of the input, then appends it to the li element created above
-// 	ul.appendChild(li);  //appends that whole thing in the new li element to the ul parent
-// 	input.value = “”;  //resets the input to null so that the user can start fresh with no characters in the entry field (not necessary therefore for user to delete the last entry they put in order to make a new entry)
+	// ul.appendChild(li);  //appends that whole thing in the new li element to the ul parent
+	// input.value = “”;  //resets the input to null so that the user can start fresh with no characters in the entry field (not necessary therefore for user to delete the last entry they put in order to make a new entry)
 
 
 // function addToListAfterClick() {     //if they click the enter button, this function activates the createListElement function
@@ -63,7 +63,7 @@
 // 	}
 // }
 
-// // the following listen to see if the button is clicked to add a list item, or if the return key is pressed. and makes sure that an item is NOT added to the list if both letters have been entered and the return key has been pressed
+// the following listen to see if the button is clicked to add a list item, or if the return key is pressed. and makes sure that an item is NOT added to the list if both letters have been entered and the return key has been pressed
 
 
 // button.addEventListener(“click”, addToListAfterClick);   //an event listener is a call-back function: there is no () after addToListAfterClick because that would call the function immediately, we want it called only when a click happens and this absence tells js to not call it immediately
