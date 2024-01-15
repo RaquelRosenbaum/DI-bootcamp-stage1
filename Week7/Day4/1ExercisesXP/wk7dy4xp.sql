33389b84cee8e1b61c5e66122ea0b91f40275cb4
RaquelRosenbaum <brightfutures7777@gmail.com>
2024-01-15 21:10:08 +0200

    wk7dy4XP

--- Week7/Day4/1ExercisesXP/wk7dy4xp.sql
+++ Week7/Day4/1ExercisesXP/wk7dy4xp.sql
@@ -7,10 +7,29 @@ INNER JOIN language ON film.language_id = language.language_id;
 
 --3
 SELECT film.title, film.description, language.name 
-FULL OUTER JOIN language ON film.language_id = language.language_id;
----couldn't make this one work, syntax error at OUTER
+FROM film 
+RIGHT JOIN language 
+ON film.language_id = language.language_id;
+
 --4
-CREATE TABLE customer_review (review_id PRIMARY NOT NULL, film_id ,
+CREATE TABLE new_film(
+	id SERIAL PRIMARY KEY NOT NULL, 
+	name UNIQUE VARCHAR(255));
+INSERT INTO new_film (name)
+VALUES ('Cadswall'),('The Epic Journey of Fjord'),('The Day the Horse Went for a Walk');
+
 --5
-
-
+CREATE TABLE customer_review( 
+		review_id SERIAL,
+		film_id INTEGER NOT NULL,
+		language_id INTEGER,
+	   	title VARCHAR(1000),
+		score SMALLINT NOT NULL CHECK(score > 0 and score <=10),
+		review_text VARCHAR(1000),
+		last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		PRIMARY KEY (review_id), 
+		FOREIGN KEY (film_id) REFERENCES new_film(id),
+		FOREIGN KEY (language_id) REFERENCES language(language_id));
+SELECT * FROM customer_review;
+ALTER TABLE ADD CONSTRAINT 
+--6
